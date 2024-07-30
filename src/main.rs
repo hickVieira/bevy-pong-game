fbc8e3d5bd2526c0239578b54fd5f049a9e71f59
hickVieira <lhlvieirag@gmail.com>
2024-07-30 11:02:42 -0300

    fixed bad init order by using .chain()

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,26 @@ use bevy::prelude::*;
 mod pong;
 
 fn main() {
-    let mut app = App::new();
-
-    app.add_plugins(DefaultPlugins);
-
-    app.add_systems(
-        Startup,
-        (
-            pong::sys_spawn_camera,
-            pong::sys_spawn_players,
-            pong::sys_spawn_ball,
-        ),
-    );
-    app.add_systems(Update, pong::sys_move_paddles);
-    app.add_systems(
-        Update,
-        (
-            pong::sys_move_ball,
-            pong::sys_collide_ball_walls,
-            pong::sys_collide_ball_paddle,
-        ),
-    );
-
-    app.run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(
+            Startup,
+            (
+                pong::sys_spawn_camera,
+                pong::sys_spawn_players,
+                pong::sys_spawn_ball,
+            )
+                .chain(),
+        )
+        .add_systems(
+            Update,
+            (
+                pong::sys_move_paddles,
+                pong::sys_move_ball,
+                pong::sys_collide_ball_walls,
+                pong::sys_collide_ball_paddle,
+            )
+                .chain(),
+        )
+        .run();
 }
